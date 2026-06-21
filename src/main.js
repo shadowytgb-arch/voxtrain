ebb31e3fa13f8eabcc15af38d615b2aa7d3e33ff
shadowytgb-arch <shadowytgb@gmail.com>
2026-06-21 20:45:41 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './styles/tokens.css';
+import './styles/base.css';
+import './styles/layout.css';
+import './styles/components.css';
+import './styles/pages/auth.css';
+import './styles/pages/play.css';
+import './styles/pages/game.css';
+import './styles/pages/leaderboard.css';
+import './styles/pages/customize.css';
+import './styles/pages/shop.css';
+import './styles/pages/progress.css';
+import './styles/pages/social.css';
+import './styles/pages/reaction.css';
+import './styles/pages/profile.css';
+import './styles/pages/updates.css';
+import { bootstrapApp } from './app/bootstrap.js';
+
+bootstrapApp(document.getElementById('app'));
